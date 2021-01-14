ce9f8c9427ecad87fa867ad037707e78c6f8c698
January <januaryobrien.ux@gmail.com>
2021-01-14 17:21:27 -0500

    fixed environment

--- planted-client-app/src/helpers/environment.tsx
+++ planted-client-app/src/helpers/environment.tsx
@@ -1,12 +1,10 @@
 let APIURL = "";
+
 switch (window.location.hostname) {
   case "localhost" || "127.0.0.1":
     APIURL = "http://localhost:4000";
     break;
-  case "jo-plantedapp-client.herokuapp.com/":
+  case "jo-plantedapp-client.herokuapp.com":
     APIURL = "https://jo-plantedapp.herokuapp.com";
-    break;
-  default:
-    APIURL = null;
 }
 export default APIURL;
