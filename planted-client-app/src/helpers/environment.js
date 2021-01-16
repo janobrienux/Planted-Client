2f46a403ece388475c28a6137fa8c6c0d2481283
January <januaryobrien.ux@gmail.com>
2021-01-16 14:42:11 -0500

    testing url

--- planted-client-app/src/helpers/environment.js
+++ planted-client-app/src/helpers/environment.js
@@ -4,7 +4,7 @@ switch (window.location.hostname) {
   case "localhost" || "127.0.0.1":
     APIURL = "http://localhost:4000";
     break;
-  case "https://planted-client.herokuapp.com/":
+  case "planted-client.herokuapp.com/":
     APIURL = "https://jo-planted.herokuapp.com/";
     break;
 
