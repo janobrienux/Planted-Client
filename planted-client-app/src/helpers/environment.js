010019b7afec18ac1361de9e88678be93d343569
January <januaryobrien.ux@gmail.com>
2021-01-16 14:13:12 -0500

    testing with new server heroku

--- planted-client-app/src/helpers/environment.js
+++ planted-client-app/src/helpers/environment.js
@@ -4,7 +4,12 @@ switch (window.location.hostname) {
   case "localhost" || "127.0.0.1":
     APIURL = "http://localhost:4000";
     break;
-  case "jo-plantedapp-client.herokuapp.com":
-    APIURL = "https://jo-plantedapp.herokuapp.com";
+  case "https://planted-client.herokuapp.com/":
+    APIURL = "https://jo-planted.herokuapp.com/";
+    break;
+
+  default:
+    APIURL = "";
 }
+
 export default APIURL;
