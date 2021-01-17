5759ff8e78b969487e042b59f53e1ad959670524
January <januaryobrien.ux@gmail.com>
2021-01-17 09:42:09 -0500

    removed slash from api url

--- planted-client-app/src/helpers/environment.js
+++ planted-client-app/src/helpers/environment.js
@@ -4,8 +4,8 @@ switch (window.location.hostname) {
   case "localhost" || "127.0.0.1":
     APIURL = "http://localhost:4000";
     break;
-  case "planted-client.herokuapp.com/":
-    APIURL = "https://jo-planted.herokuapp.com/";
+  case "planted-client.herokuapp.com":
+    APIURL = "https://jo-planted.herokuapp.com";
     break;
 
   default:
